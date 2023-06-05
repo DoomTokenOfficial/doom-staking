ab1423c56d8e2ad0c4d822e74a68795a2271727f
TOP <131230649+strongman20021104@users.noreply.github.com>
2023-06-05 08:50:45 +0000

    fix(constants): Update RPC URL and supportedChainIds for Fantom testnet

--- src/assets/constants/connectors.js
+++ src/assets/constants/connectors.js
@@ -2,15 +2,15 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 const POLLING_INTERVAL = 12000;
-const RPC_URL = "https://bsc-dataseed.binance.org/";
+const RPC_URL = "https://rpc.testnet.fantom.network/";
 // const RPC_URL = "https://data-seed-prebsc-1-s1.binance.org:8545";
 
 export const injected = new InjectedConnector({
-    supportedChainIds: [56],
+    supportedChainIds: [4002],
 });
 
 export const walletconnect = new WalletConnectConnector({
-    rpc: { 56: RPC_URL },
+    rpc: { 4002: RPC_URL },
     bridge: "https://bridge.walletconnect.org",
     qrcode: true,
     pollingInterval: POLLING_INTERVAL,
